5e1ec4130e9419ba43637a62016f8c8f9f2d2f02
nicolasjutge-debug <nicolas.jutge@gmail.com>
2026-07-05 19:42:51 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-// Moteur VertiBalance
-document.addEventListener("DOMContentLoaded", () => {
-    const binId = localStorage.getItem("VERTIBALANCE_BIN_ID");
-    if (!binId) {
-        console.log("Configuration nécessaire via generation.html");
-    } else {
-        console.log("Profil chargé:", binId);
-    }
-});
+console.log('App démarrée');
